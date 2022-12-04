4670b18efda319f1353052de5c9c05968c884dbe
Teamjbworks <116152922+Teamjbworks@users.noreply.github.com>
2022-12-04 18:11:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+//Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+function pay(){
+  window.open("upi://pay?pn=PaperBoatGraphics&amp;pa=paperboat4u@oksbi")
+}
+function btn(){
+  window.open("developmen fund.html")
+}
+function btn1(){
+function 
+  generatePassword(passwordLength) {
+  var numberChars = "0123456789";
+  var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+  var allChars = numberChars + upperChars + lowerChars;
+  var randPasswordArray = Array(passwordLength);
+  randPasswordArray[0] = numberChars;
+  randPasswordArray[1] = upperChars;
+  randPasswordArray[2] = lowerChars;
+  randPasswordArray = randPasswordArray.fill(allChars, 3);
+  return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
+}
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+alert(" Read it carefully!!! Screenshot or Copy this code and show or sent to 'Vilakkupara sree mahadevar maha shketram' to complete this process ! ")
+alert(generatePassword(5));
+
+}
